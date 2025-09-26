629662794a6f9839b57803828c4b0e44647ebc20
QwErT707 <bloodyghosttt777@gmail.com>
2025-09-26 22:14:49 +0400

    Added Kafka, basic functions for MS-1, MS-2 and MS-3: - Implemented new client registration with blacklist checks. - Configured product management (CRUD) and client portfolio product management (CRUD), with messages sent to the client_products and client_credit_products topics. - Implemented processing of card creation requests and sending messages to the client_cards topic. - Implemented listening to the client_products, client_transactions, and client_cards topics to automatically create products and cards upon receiving messages. - Added the monthCount attribute to the ProductRegistry entity. - Implemented message processing logic for topics, including limit checking, product creation, annuity payment calculation (example with calculation for a 1.5 million ruble loan for 5 years at 22%), and approval or denial decisions.

--- account_processing/src/main/resources/db/migration/V2__create_cards_table.sql
+++ account_processing/src/main/resources/db/migration/V2__create_cards_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE cards (
     id BIGSERIAL PRIMARY KEY,
     account_id BIGINT NOT NULL,
-    card_id VARCHAR(20) NOT NULL UNIQUE,
+    card_id VARCHAR(36) NOT NULL UNIQUE,
     payment_system VARCHAR(20) NOT NULL CHECK (payment_system IN ('VISA', 'MASTERCARD', 'MIR', 'AMEX')),
     status VARCHAR(20) NOT NULL CHECK (status IN ('ACTIVE', 'BLOCKED', 'CANCELLED')),
 
